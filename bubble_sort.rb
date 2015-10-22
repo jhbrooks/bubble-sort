e483d6c138850ef6a19be424315282e2ef2ecdce
James Brooks <jhbrooks@uchicago.edu>
2015-10-22 00:04:56 -0400

    implement #bubble_sort_by

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -13,3 +13,21 @@ def bubble_sort(array)
 		bubble_sort(array)
 	end
 end
+
+def bubble_sort_by(array, &block)
+	unbubbled_array = array + []
+
+	0.upto(array.length - 2) do |index| # -2 instead of -1 to avoid last index
+		# yields to block to define comparison
+		# block should return > 0 if left item belongs after right item
+		if yield(array[index], array[index + 1]) > 0
+			array[index], array[index + 1] = array[index + 1], array[index]
+		end
+	end
+
+	if unbubbled_array == array
+		return array
+	else
+		bubble_sort_by(array,&block)
+	end
+end
